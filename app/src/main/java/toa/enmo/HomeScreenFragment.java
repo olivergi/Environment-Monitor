90a8bddc9ff8b91303e4974fab2251afc12673a0
Oliver <oliver.cj.gill@gmail.com>
2016-09-23 09:36:04 +0300

    Recent Commit

--- app/src/main/java/toa/enmo/HomeScreenFragment.java
+++ app/src/main/java/toa/enmo/HomeScreenFragment.java
@@ -1,15 +1,19 @@
 package toa.enmo;
 
-import android.support.v4.app.Fragment;
+import android.app.FragmentTransaction;
+import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class HomeScreenFragment extends Fragment {
+
+    AnalysisFragment af;
 
     public HomeScreenFragment() {
     }
@@ -17,6 +21,19 @@ public class HomeScreenFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_fragment, container, false);
+        View v = inflater.inflate(R.layout.home_fragment, container, false);
+        Button button = (Button) v.findViewById(R.id.btn3);
+        af = new AnalysisFragment();
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                FragmentTransaction fm = getFragmentManager().beginTransaction();
+                fm.replace(R.id.frag_container, af);
+                fm.commit();
+            }
+        });
+
+        return v;
     }
 }
